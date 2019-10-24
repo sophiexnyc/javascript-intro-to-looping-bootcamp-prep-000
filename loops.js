f5861a650bbdfc1a2b2e84c4901d91f09c2e7d10
Sophie Lee <sblee092@gmail.com>
2019-10-24 00:56:00 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,35 @@
+function forLoop (array)
+{
+  for (let i=0; i<25;i++)
+  {
+    //
+    if(i === 0)
+    {
+      array[i]= "I am 1 strange loop.";
+    }
+    else if ()
+    {
+      array[i]= "I am ${i} strange loops.";
+    }
+  }
+  return array;
+}
+
+function whileLoop(n)
+{
+  while (n>0)
+  {
+    n=n-1
+    console.log(n);
+  }
+  return 'done';
+}
+
+function doWhileLoop(num)
+{
+  do
+  {
+    num=num-1;
+    console.log ("I run once regardless.");
+  } while(num>0);
+}
