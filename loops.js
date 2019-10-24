86da65241ecd129640fc4c140c2e35a1781e7241
Sophie Lee <sblee092@gmail.com>
2019-10-24 01:41:12 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,17 +1,10 @@
 function forLoop (array)
 {
-  for (let i=0; i<25;i++)
-  {
-    if(i === 0)
-    {
-      array.push ("I am 1 strange loop.");
-    }
-    else
-    {
-      array.push (`I am ${i+1} strange loops.`);
-    }
-  }
-  return array;
+  for (let i=0; i<25;i++)
+  {
+    array.push(`I am ${i} strange loop${i === 1 ? '' : 's'}.`)
+  }
+  return array;
 }
 
 function whileLoop(n)
